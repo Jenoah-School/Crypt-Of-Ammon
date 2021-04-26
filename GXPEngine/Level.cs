3b52f5eb911431968ad2959c2e4c24babc067112
Jenoah Kers <hello@jenoah.nl>
2021-04-26 11:00:34 +0200

    Added level class and SwitchLevel method

---
+++ GXPEngine/Level.cs
@@ -0,0 +1,21 @@
+﻿using GXPEngine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Level : GameObject
+{
+    protected List<Entity> sceneObjects = new List<Entity>();
+
+    public virtual void Load()
+    {
+
+    }
+
+    public virtual void Unload()
+    {
+
+    }
+}
--- GXPEngine/MyGame.cs
+++ GXPEngine/MyGame.cs
@@ -9,16 +9,44 @@ public class MyGame : Game
 {
 	public static List<Collider> collisionObjects = new List<Collider>();
 
+	public Level currentLevel { get; private set; }
+	private List<Level> levels = new List<Level>();
+
 	public MyGame() : base(1280, 720, false, false)	
 	{
 		GL.glfwSetWindowPos((Screen.PrimaryScreen.Bounds.Width - width) / 2, (Screen.PrimaryScreen.Bounds.Height - height) / 2);
 		GL.glfwSetWindowTitle("Final Approach");
 
 		targetFps = 60;
+
+		//Just temp to test
+		levels.Add(new Level());
+
+		SwitchLevel(0);
 	}
 
 	static void Main()
 	{
 		new MyGame().Start();
 	}
+
+	public void SwitchLevel(int _levelId)
+    {
+		if(_levelId < levels.Count) 
+        {
+			if (currentLevel != null)
+			{
+				currentLevel.Unload();
+				currentLevel.LateDestroy();
+				currentLevel = null;
+			}
+			currentLevel = levels[_levelId];
+			currentLevel.Load();
+			AddChild(currentLevel);
+        }
+        else
+        {
+			Console.WriteLine($"Level ID ({_levelId}) does not correspond to an existing level (from {levels.Count} levels)");
+        }
+    }
 }
